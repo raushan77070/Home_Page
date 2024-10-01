53841c84e90e3951fa4b09c7529753b508e407fe
Raushan Raj <169126313+raushan77070@users.noreply.github.com>
2024-10-01 21:14:06 +0530

    ready for deployment

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+
+// tailwind.config.js
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: 'media', // This is fine as it is
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
